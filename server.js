48add5214f3480b2c8efd9711e22970a098c8600
Alvin Ng <ngalvin93@gmail.com>
2019-09-17 21:24:49 -0500

    progress on api

--- server.js
+++ server.js
@@ -11,13 +11,28 @@ var todoList = [
         todo: "Implement a REST API"
     }
 ];
-
+// Home page
+app.get('/', function (request, response, next) {
+    response.send('What up')
+})
 // GET /api/todos
-
+app.get('/api/todos', function (request, response, next) {
+    response.send(todoList)
+})
 // GET /api/todos/:id
-
+app.get('/api/todos/:id', function (request, response, next) {
+    //console.log(request.params.id) // this is what you typed into the url to provide the id
+    const theUserId = todoList.find(function (user) {
+        return user.id.toString() === request.params.id.toString() // must convert to string becuase comparing number to string
+    })
+    // console.log(request.params)
+    response.send(theUserId)
+    next()
+})
 // POST /api/todos
-
+app.post('/api/todos', function (request, response, next) {
+    
+})
 // PUT /api/todos/:id
 
 // DELETE /api/todos/:id
